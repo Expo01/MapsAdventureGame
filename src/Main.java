d98e8b39f5db8f256b349fc17e771bbd0edae61f
Expo <crdailey01@gmail.com>
2023-02-26 10:09:08 -0800

    annotated first version, main map created, class map no content yet.

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static Map<Integer, Location> locations = new HashMap<>();
+    //locations map accepts Location instance as value, consisting of locationID and description
 
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 
         //note autoboxing of Integer
+        //new location generated
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
         locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
         locations.put(2, new Location(2, "You are at the top of a hill"));
@@ -16,6 +18,10 @@ public class Main {
         locations.put(4, new Location(4, "You are in a valley beside a stream"));
         locations.put(5, new Location(5, "You are in the forest"));
 
+      //while the Location class has a map of exits, not key/values have been put into the map. this content only loops
+        // through the locations created in the 'locations' map. The result is that we can go directly from any location,
+        //to any location, which doesn't reflect real life where areas between areas must be traversed and some ways you
+        //aren't allowed backwards like airport security
         int loc = 1;
         while(true) {
             System.out.println(locations.get(loc).getDescription());
